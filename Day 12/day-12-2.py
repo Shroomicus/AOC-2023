f77b3e7da21dcfa59af2aea5f44277c2b1daeab2
Shroomicus <linvincent969@gmail.com>
2023-12-12 18:24:25 -0500

    continue

--- Day 12/day-12-2.py
+++ Day 12/day-12-2.py
@@ -3,14 +3,20 @@ lines = input.readlines()
 
 def getNext(unknown, expected, state):
     next = []
-    if(unknown[state[0]] != '.'):
-        if(state[1] < expected[state[2]]):
-            next.append((state[0]+1, state[1]+1, state[2]))
-    if(unknown[state[0]] != '#'):
-        if(state[1] > 0):
+    if(unknown[state[0]] == '.'):
+        if(state[1] == expected[state[2]]):
             next.append((state[0]+1, 0, state[2]+1))
         else:
-            next.append((state[0]+1, state[1], state[2]))
+            next.append((state[0]+1, 0, state[2]))
+    if(unknown[state[0]] == '#'):
+        if(state[1] < expected[state[2]]):
+            next.append((state[0]+1, state[1]+1, state[2]))
+    if(unknown[state[0]] == '?'):
+        if(state[1] < expected[state[2]]):
+            next.append((state[0]+1, state[1]+1, state[2]))
+        elif(state[1] == expected[state[2]]):
+            next.append((state[0]+1, 0, state[2]+1))
+        next.append((state[0]+1, 0, state[2]))
     
     return next
 
@@ -27,6 +33,7 @@ for line in lines[1:2]:
     unknown, expected = line.replace('\n', '').split()
     unknown = "?".join(unknown for _ in range(5))
     expected = tuple(int(x) for x in expected.split(','))*5
+    # expected = tuple(int(x) for x in expected.split(','))
     print(unknown)
     print(str(expected) + "\n")
 
@@ -37,12 +44,13 @@ for line in lines[1:2]:
 
     while(len(queue) > 0):
         curr = queue.pop(0)
-        print(curr)
+        # print(curr)
         neighbors = getNext(unknown, expected, curr)
         for i in neighbors:
-            # print(i[0])
             if(i[0] == len(unknown)-1):
-                res += 1
+                if(i[2] == len(expected)-1):
+                    if(i[1] == expected[-1]):
+                        res += 1
                 continue
             queue.append(i)
         # print()
