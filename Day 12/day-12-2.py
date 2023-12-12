d5ad4dc456f15d83fcf6a602602f3f577ec98821
Shroomicus <linvincent969@gmail.com>
2023-12-12 18:25:46 -0500

    AAAA

--- Day 12/day-12-2.py
+++ Day 12/day-12-2.py
@@ -27,13 +27,13 @@ def knownData(line):
     return [x.count('#') for x in line.replace('.', ' ').split()]
 
 res = 0
-for line in lines[1:2]:
+for line in lines:
     queue = []
     total = 0
     unknown, expected = line.replace('\n', '').split()
-    unknown = "?".join(unknown for _ in range(5))
-    expected = tuple(int(x) for x in expected.split(','))*5
-    # expected = tuple(int(x) for x in expected.split(','))
+    # unknown = "?".join(unknown for _ in range(5))
+    # expected = tuple(int(x) for x in expected.split(','))*5
+    expected = tuple(int(x) for x in expected.split(','))
     print(unknown)
     print(str(expected) + "\n")
 
